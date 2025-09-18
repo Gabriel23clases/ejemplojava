8259543a9806ff1c47db3563b1a09c1bc8e1a6dd
Gabriel23clases <garcia.lobo.gabriel@iescamas.es>
2025-09-18 19:22:58 +0200

    Add files via upload

---
+++ TipoDeDato.java
@@ -0,0 +1,18 @@
+package com;
+
+public class TipoDeDato {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero = 100;
+		float numero_decimal = (float) 1.1F;
+		double numero_decimal2 = 1.1;
+		boolean logico = false;
+		String palabra = "Tomate";
+		char letra = '1';
+		System.out.println("Hola)")	;
+		System.out.println ("numero decimal: " + numero_decimal);
+		System.out.println ("Logico " + logico);
+	}
+
+}
---
+++ lectura.java
@@ -0,0 +1,13 @@
+package com;
+import java.util.Scanner;
+public class lectura {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+System.out.println("dime tu nombre: ");
+String nombre = sc.nextLine();
+System.out.println ("Hola " + nombre);
+	}
+
+}
