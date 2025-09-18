4222f69a08119d6a5c2a447a443128c66dd2b20c
Gabriel23clases <garcia.lobo.gabriel@iescamas.es>
2025-09-18 20:03:25 +0200

    Add files via upload

---
+++ code/Actividad2.java
@@ -0,0 +1,13 @@
+package actividadesUnidad1;
+
+public class Actividad2 {
+import java.util.Scanner;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner lector = new Scanner (System.in);
+	System.out.println ("Introduzca su número: ");
+	String numero = lector.nextLine();
+	System.out.println ("numero");
+	}
+
+
