c35dc39719d5d54140e7fd9113c216c9ca941e16
Gabriel23clases <garcia.lobo.gabriel@iescamas.es>
2025-09-23 21:04:23 +0200

    Add files via upload

--- code/Actividad1.java
+++ code/Actividad1.java
@@ -1,16 +1,9 @@
-package actividadesUnidad2;
-import java.util.Scanner;
+package actividadesUnidad1;
+
 public class Actividad1 {
-//Diseñar una aplicación que solicite al usuario un número e indique si es par o impar.
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num;
-		System.out.println("Dime un numero entero");
-		num = sc.nextInt();
-		//usamos % para resto
-		if (num % 2 == 0)
-		System.out.println("su número es par");
-		else System.out.println("su número es impar");
+		System.out.println("Hola Mundo");
 	}
 
 }
